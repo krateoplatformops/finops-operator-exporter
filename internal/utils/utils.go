65042d41f64d5d6faa05baa0f9e0baef590a721b
FrancescoL96 <francesco29.fl@gmail.com>
2025-01-30 16:09:15 +0100

    fix: exporter version is now parsed from environment

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -22,10 +22,12 @@ func Int32Ptr(i int32) *int32 { return &i }
 
 func GetGenericExporterDeployment(exporterScraperConfig *finopsv1.ExporterScraperConfig) (*appsv1.Deployment, error) {
 	imageName := strings.TrimSuffix(os.Getenv("REGISTRY"), "/")
+	imageVersion := strings.TrimSuffix(os.Getenv("EXPORTER_VERSION"), "latest")
+	resourceImageVersion := strings.TrimSuffix(os.Getenv("RESOURCE_EXPORTER_VERSION"), "latest")
 	if strings.ToLower(exporterScraperConfig.Spec.ExporterConfig.MetricType) == "resource" {
-		imageName += "/finops-prometheus-resource-exporter-azure:latest"
+		imageName += "/finops-prometheus-resource-exporter-azure: " + resourceImageVersion
 	} else {
-		imageName += "/finops-prometheus-exporter-generic:latest"
+		imageName += "/finops-prometheus-exporter-generic:" + imageVersion
 	}
 
 	deployment := &appsv1.Deployment{
